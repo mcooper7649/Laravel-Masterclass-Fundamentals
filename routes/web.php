82d2a47c2c7e651bd38ef7544d7b1b6f7dc0db2b
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2024-08-13 12:48:43 -0400

    routes, expressions, complex expressions, multiple values, contraint methods and named routes

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,85 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Http\request;
+use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function (Request $request) {
 //     return redirect('/profile');
 // });
 
-Route::redirect('/', '/profile');
+// Route::redirect('/', '/profile');
 
-Route::get('/profile', function (Request $request) {
-    return "PROFILE";
-});
+// Route::get('/profile', function (Request $request) {
+//     return "PROFILE";
+// });
 
-Route::get('/{id}/{status}', function ($id, $status) {
-    return $status;
-});
+// Route::get('/{id}/{status}', function ($id, $status) {
+//     return $status;
+// });
 
 //optional parameter routes have a ?
 
-Route::get('/{id?}', function (?int $id = null) {
-    return $id;
-});
+// Route::get('/{id?}', function (?int $id = null) {
+//     return $id;
+// });
 
+// Route::get('/user/{id}', function ($id) {
+//     return $id;
+// })->where('id', 23);
+
+//Advanced routes that handle uppercase routes
+// Route::get('/user/{name}', function ($name) {
+//     return $name;
+// })->where('name', '[A-Za-z]+');
+
+//undocumented way of passing a specific route
+
+// Route::get('/user/{name}/{id}', function ($name) {
+//     return $name;
+// })->where('name', '[A-Za-z]+')->where('id', 23);
+
+//documented way of passing a specific route
+
+// Route::get('/user/{name}/{id}', function ($name) {
+//     return $name;
+// })->where(['name', '[A-Za-z]+', 'id' => 23]);
+
+//documented way of passing all ids
+
+// Route::get('/user/{name}/{id}', function (string $name, int $id) {
+//     return $name;
+// })->where(['name', '[A-Za-z]+', 'id' => '[0-9]+']);
+
+//Docemented Where commands to do the same things
+
+// Route::get('/user/{name}', function ($name) {
+//     return $name;
+// })->whereAlpha('name');
+
+// Route::get('/user/{name}', function ($name) {
+//     return $name;
+// })->whereNumber('name');
+
+// Route::get('/user/{categories}', function ($categories) {
+//     return $categories;
+// })->whereIn('categories', ['php', 'python', 'java']);
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::get('/company-contactfl;kajsdkflja;s', function () {
+    return 'contact';
+})->name('company.contact');
+
+Route::get('/profile', function () {
+    return view('welcome');
+})->name('profile');
 
 // Route::get('/user', 'CONTROLLER', 'METHOD');
 // Route::get('/posts', 'CONTROLLER', 'METHOD');
 // Route::get('/categories', 'CONTROLLER', 'METHOD');
 // Route::get('/chat', 'CONTROLLER', 'METHOD');
-
 
 // Route::post('/', function () {
 //     return view('welcome');
@@ -49,5 +100,3 @@ Route::get('/{id?}', function (?int $id = null) {
 // Route::options('/', function () {
 //     return view('welcome');
 // });
-
-
